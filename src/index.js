8774939e4e834e437740f8295edb7191dae49027
PaulPonomarenko <pashaponomarenkoa1999@gmail.com>
2023-04-25 12:18:09 +0300

    добавляет оформление

--- src/index.js
+++ src/index.js
@@ -1,18 +1,58 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const input = document.querySelector('#search-box');
 const countryContainer = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 const DEBOUNCE_DELAY = 300;
-const countryResult = fetchCountries();
-console.log(countryResult);
+
 input.addEventListener('input', debounce(onEvent, DEBOUNCE_DELAY));
 function onEvent(event) {
-  fetchCountries(input.value).then(renderCountriesName);
+  const country = input.value.trim();
+  if (country.length === 0) {
+    countryContainer.innerHTML = '';
+    countryList.innerHTML = '';
+    return;
+  }
+  fetchCountries(country)
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length >= 2 && data.length <= 10) {
+        countryContainer.innerHTML = '';
+        renderFlagName(data);
+      } else if (data.length === 1) {
+        countryList.innerHTML = '';
+        renderCountriesName(data);
+      }
+    })
+    .catch(onError);
+}
+
+function onError(error) {
+  if (error.message === 'Not Found') {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+  }
+}
+
+function renderFlagName(data) {
+  const markupList = data
+    .map(({ flags, name }) => {
+      return `<li class = "li-item">
+                <img class ="country-flag" src ="${flags.svg}" alt = "${flags.alt} " >
+                <h3 class = "country-name-low">${name.official}</h3>
+                </li>`;
+    })
+    .join('');
+  countryList.innerHTML = markupList;
 }
 
 function renderCountriesName(data) {
-  const markup = data
+  const markupContainer = data
     .map(({ name, flags, capital, population, languages }) => {
       return `<div class = "country-header">
                 <img class ="country-flag" src ="${flags.svg}" alt = "${
@@ -33,9 +73,9 @@ function renderCountriesName(data) {
                 <b>Languages:</b>
                 <span class="result-span">${Object.values(languages)}</span>
             </li>
-            
+
             </ul>`;
     })
     .join('');
-  countryContainer.innerHTML = markup;
+  countryContainer.innerHTML = markupContainer;
 }
